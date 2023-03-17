ff35c427581be2c0588ca2418f7153598c09351c
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-17 15:18:10 +0000

    Initial commit

---
+++ palindrome_in_javascript.js
@@ -0,0 +1,31 @@
+
+
+
+
+function isPalindrome(x) {
+// Covering case for negative values as they are not palindrome
+  if (x < 0) {
+
+    return false;
+  }
+
+  // storing value of x in another variable as we are changing it inside loop
+  let k = x;
+  let rev = 0;
+  // rev to store reverse value
+  while (x != 0) {
+// x%10 to store last degit rev *10 to move previous value to last position
+    rev = x % 10 + rev * 10;
+
+    x = Math.floor(x / 10);
+//  math.floor to not consider decimal value
+
+
+  }
+  
+
+  // returning true or false from below
+  return rev == k;
+
+
+}
